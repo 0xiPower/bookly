d40f599ac3c7b5cf51615c6130d9c752894723c0
Power <powerleecn@foxmail.com>
2025-03-20 21:26:58 +0800

    Add comment functionality, create comment routing, support comment access for user and administrator roles, enhance comment services, update error handling and logging

--- src/reviews/routes.py
+++ src/reviews/routes.py
@@ -5,13 +5,28 @@ from .schemas import ReviewCreateModel
 from .service import ReviewService
 from src.db.models import User
 from src.db.main import get_session
-from src.auth.dependencies import get_current_user
+from src.auth.dependencies import get_current_user, RoleChecker
 
 review_router = APIRouter()
 review_service = ReviewService()
+admin_role_checker = Depends(RoleChecker(["admin"]))
+user_role_checker = Depends(RoleChecker(["user", "admin"]))
 
 
-@review_router.post("/book/{book_uid}")
+@review_router.get("", dependencies=[admin_role_checker])
+async def get_all_reviews(session: AsyncSession = Depends(get_session)):
+    books = await review_service.get_all_reviews(session)
+    return books
+
+
+@review_router.get("/{review_uid}", dependencies=[user_role_checker])
+async def get_review(review_uid: str, session: AsyncSession = Depends(get_session)):
+    book = await review_service.get_review(review_uid, session=session)
+    if not book:
+        raise
+
+
+@review_router.post("/book/{book_uid}", dependencies=[user_role_checker])
 async def add_review_to_book(
     book_uid: str,
     review_data: ReviewCreateModel,
@@ -25,3 +40,19 @@ async def add_review_to_book(
         session=session,
     )
     return new_review
+
+
+@review_router.delete(
+    "/{review_uid}",
+    dependencies=[user_role_checker],
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_review(
+    review_uid: str,
+    current_user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(get_session),
+):
+    await review_service.delete_review_to_from_book(
+        review_uid=review_uid, user_email=current_user.email, session=session
+    )
+    return None
